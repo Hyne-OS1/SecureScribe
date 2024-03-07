b506f4a6204990790f3ec13366ce9a454b0ab526
AnthonyBonanno <bonant12@gmail.com>
2024-03-07 20:26:44 +1100

    added auth code

--- untils/auth.js
+++ untils/auth.js
@@ -1,2 +1,10 @@
-// user authentication file this will contain "withAuth" variable function to check if user is logged in or not, if not redirect 
-// ensure to export file at end of code to make it accessible
+const withAuth = (req, res, next) => {
+    // If the user is not logged in, redirect the request to the login route
+    if (!req.session.logged_in) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
