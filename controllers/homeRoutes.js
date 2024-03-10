ad340648b6a41142d91d7ad0b3504088c102d454
eddiespag-hetti <eddie_p89@hotmail.com>
2024-03-10 13:59:07 +1100

    added routes, models and server.js functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,23 +1,87 @@
 // const require variables for router, models, utils/auth
 const router = require('express').Router();
+const { Scribe, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 
 // GET request for root URL ('/')
+router.get('/', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const scribeData = await Scribe.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+      // Serialize data so that it is suitable to be rendered in templates
+      const scribe = scribeData.map((scribes) => scribes.get({ plain: true }));
+      // Pass serialized data and session flag into template
+      res.render('homepage', {
+        scribe,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 
-router.get('/', (req, res) => {
-    if (!req.session.loggedIn) {
-        res.redirect('/login');  
-        return; 
-    } else {
-      req.session.save(() => {
-          res.render('homepage', {
-              user_name: req.session.user_name,
-              logged_in: req.session.loggedIn
+  router.get('/scribe/:id', async (req, res) => {
+    try {
+      const scribeData = await Scribe.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+
+  
+
+
+
+      router.get('/scribe', withAuth, async (req, res) => {
+        try {
+          // Find the logged in user based on the session ID
+          const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Project }],
           });
-      })
-    };
-});
+          const user = userData.get({ plain: true });
+          res.render('scribe', {
+            ...user,
+            logged_in: true
+          });
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      });
+      router.get('/login', (req, res) => {
+        // If the user is already logged in, redirect the request to another route
+        if (req.session.logged_in) {
+          res.redirect('/profile');
+          return;
+        }
+        res.render('login');
+      });
+  
+  
+  
+  
+  const scribe = ScribeData.get({ plain: true });
+      res.render('Scribe', {
+        ...project,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 // define route
 // handle request
 // try catch block
@@ -37,3 +101,5 @@ router.get('/', (req, res) => {
 // handle request
 // try catch block
 // find user data
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -4,4 +4,14 @@
 // in this examples require API folder and Homeroutes file
 // these can be updated for other routes as needed.
 
+const router = require("express").Router();
+const userRoutes = require('./user-routes');
+const scribeRoutes = require("./scribe-routes")
+const homeRoutes = require("./homeRoutes");
+
+router.use("/", homeRoutes);
+router.use("/user-routes", userRoutes);
+router.use("/scribe-routes", scribeRoutes);
+
 // module.exports = router;
+module.exports = router;
