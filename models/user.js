c46dc8d3edad94b5b9788a7f1e4b3c8b58a80f89
AnthonyBonanno <bonant12@gmail.com>
2024-03-10 15:20:16 +1100

    added models and seeds

--- models/user.js
+++ models/user.js
@@ -3,3 +3,62 @@
 // populate user data, id, name etc with values and primarykey
 // password bcrypt hashing will also take place in this file in a hook with async await function. BEFORECREATE AND BEFOREUPDATE function.
 // mopdule.exports = User;
+
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,4 +1,4 @@
-// seeds file wil use both sequelize package and require root path to config connection.js
+// seeds file will use both sequelize package and require root path to config connection.js
 
 // const variable to grab user data. require models file location.
 
@@ -7,3 +7,29 @@
 
 
 // const variable ---> async await function ---- seedDatabase(); at end
+
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
