f2838ea5d44e2eeb50c0222183a875898cd5db50
eddiespag-hetti <eddie_p89@hotmail.com>
2024-03-17 10:24:52 +1100

    added class to log out button

--- public/js/user.js
+++ public/js/user.js
@@ -17,43 +17,50 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+const createAccountBtn = document.getElementById('create-an-account');
 
-// Create account button event listener on create account page
-    const createAccountBtn = document.getElementById('create-an-account');
-
-    createAccountBtn.addEventListener('click', async (event) => {
-        event.preventDefault();
-        const name = document.querySelector('#name').value;
-        const email = document.querySelector('#email').value;
-        const password = document.querySelector('#password').value;
-      
-        try{
-           
-            const response = await fetch('/api/users/signup', {
-                method: 'POST',
-                body: JSON.stringify({
-                    name,
-                    email,
-                    password
-                    
-                }),
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            });
+// Create Account functionality
+createAccountBtn.addEventListener('click', async (event) => {
+    event.preventDefault();
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+  
+    try {
+        const response = await fetch('/api/users/signup', {
+            method: 'POST',
+            body: JSON.stringify({
+                name,
+                email,
+                password
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            }
             
-            if (response.ok) { alert('you have successfully created an account!!')
-                document.location.replace('/profile');
-                //window.location.href = '/profile';
-            } else {
-                alert("Error!!")
-                //console.error('Error creating new account!', response.statusText);
-            }
-            }
-            catch (error) {
-                console.error('Network error:', error);
-            }
+            });
+                if (response.ok) { alert('you have successfully created an account!!')
+                    document.location.replace('/profile');
+                    //window.location.href = '/profile';
+                } else {
+                    alert("Error!!")
+                    
+                    //console.error('Error creating new account!', response.statusText);
+                }
+                }
+                catch (error) {
+                    console.error('Network error:', error);
+                }
+            
+        
         });
+
+
+
+
+
+
+        
 
 
 
